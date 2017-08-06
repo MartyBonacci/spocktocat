57e55921c31240f7a219093324e287e290639e32
Marty Bonacci <martybonacci@gmail.com>
2017-08-06 16:05:41 -0600

    testing layout again......

--- index.php
+++ index.php
@@ -49,11 +49,11 @@
 
 		<div class="row">
 			<div class="col-md-2">
-				<h1></h1>
+				<br/>
+				<br/>
+				<br/>
 				<br/>
 				<h1>7</h1>
-				<h1></h1>
-				<h1></h1>
 			</div>
 			<div class="col-md-3">
 				<h1>+</h1>
@@ -69,7 +69,10 @@
 				<h1>5</h1>
 			</div>
 			<div class="col-md-5">
-				<h1></h1>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
 				<h1>= </h1>
 			</div>
 		</div>
