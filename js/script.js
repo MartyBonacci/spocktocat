3fd0d1d2c28c50fedf1c9f83c45a1f2f4d81d13e
Marty Bonacci <martybonacci@gmail.com>
2017-08-09 09:33:58 -0600

    trying assigning values to first and second value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // changes the value of the answer displayed
 var result = document.getElementById("answer");
+var firstValue = 7;
+var secondValue = 9;
+
 
 // handles the calculations and display of the answer when mouse enter addition symbol
 var plus = document.getElementById("add");
