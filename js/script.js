097f75cdf1ef728280879f21a8755b25d5f08084
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-09 14:18:06 -0600

    cleaned up one messy room

--- js/script.js
+++ js/script.js
@@ -3,29 +3,24 @@
 var result = document.getElementById("answer");
 
 // handles the calculations and display of the answer when mouse enter addition symbol
-var plus = document.getElementById("add");
-var onPlusEnter = function() {
+
+function onPlusEnter() {
 	var total = firstValue + secondValue;
 	result.textContent = "=" + total;
-};
-plus.addEventListener("mouseenter", onPlusEnter);
+}
 
 // handles the calculations and display of the answer when mouse enter take away symbol
-var takeAway = document.getElementById("subtract");
-var onMinusEnter = function() {
+function onMinusEnter() {
 	var total = firstValue - secondValue;
 	result.textContent = "=" + total;
-};
-takeAway.addEventListener("mouseenter", onMinusEnter);
+}
 
 
 // handles the calculations and display of the answer when mouse enter guzinto symbol
-var goesInto = document.getElementById("divide");
-var onDivisionEnter = function() {
+function onDivisionEnter() {
 	var total = firstValue / secondValue;
 	result.textContent = "=" + total;
-};
-goesInto.addEventListener("mouseenter", onDivisionEnter);
+}
 
 // handles the calculations and display of the answer when mouse enter multiplication symbol
 
@@ -38,21 +33,30 @@ function onMultiplyEnter () {
 	console.log(secondValue);
 	var total = firstValue * secondValue;
 	result.textContent = total;
-
-};
-
+}
 
 
 
-function applyFirstValue("test1") {
+
+function applyFirstValue() {
 	var firstValue = document.getElementById("test1");
 	var entered = document.getElementById("firstNumber").value;
 	firstValue.textContent = entered;
-};
+}
 
 
 function applySecondValue() {
 	var secondValue = document.getElementById("test2");
 	var entered = document.getElementById("secondNumber").value;
 	secondValue.textContent = entered;
+}
+
+window.onload = function() {
+	var add = document.getElementById("add");
+	var takeAway = document.getElementById("subtract");
+	var goesInto = document.getElementById("divide");
+
+	add.addEventListener("mouseenter", onPlusEnter);
+	takeAway.addEventListener("mouseenter", onMinusEnter);
+	goesInto.addEventListener("mouseenter", onDivisionEnter);
 };
