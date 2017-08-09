6958f2f7e06a400546d1733460041ab8b0938cc8
Marty Bonacci <martybonacci@gmail.com>
2017-08-09 16:01:25 -0600

    added some documentation to script file

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 // accessor for the first value
 function getFirstValue() {
 	var firstValue = parseFloat(document.getElementById("firstNumber").value);
-	return(firstValue);
+	return(firstValue); // return is used to return value when a function is called
 }
 
 // accessor for second value
 function getSecondValue() {
 	var secondValue = parseFloat(document.getElementById("secondNumber").value);
-	return(secondValue);
+	return(secondValue); // return is used to return value when a function is called
 }
 
 // mutator for answer
@@ -21,7 +21,7 @@ function onPlusEnter() {
 	var firstValue = getFirstValue();
 	var secondValue = getSecondValue();
 	var total = firstValue + secondValue;
-	setAnswer(total);
+	setAnswer(total); // calls mutator function which sends the total out to the universe
 }
 
 // handles the calculations and display of the answer when mouse enter take away symbol
@@ -29,16 +29,15 @@ function onMinusEnter() {
 	var firstValue = getFirstValue();
 	var secondValue = getSecondValue();
 	var total = firstValue - secondValue;
-	setAnswer(total);
+	setAnswer(total); // calls mutator function which sends the total out to the universe
 }
-
 
 // handles the calculations and display of the answer when mouse enter guzinto symbol
 function onDivisionEnter() {
 	var firstValue = getFirstValue();
 	var secondValue = getSecondValue();
 	var total = firstValue / secondValue;
-	setAnswer(total);
+	setAnswer(total); // calls mutator function which sends the total out to the universe
 }
 
 // handles the calculations and display of the answer when mouse enter multiplication symbol
@@ -46,9 +45,10 @@ function onMultiplyEnter () {
 	var firstValue = getFirstValue();
 	var secondValue = getSecondValue();
 	var total = firstValue * secondValue;
-	setAnswer(total);
+	setAnswer(total); // calls mutator function which sends the total out to the universe
 }
 
+// puts starting value in input boxes to prevent error on load... and to make it look cool
 window.onload = function() {
 	document.getElementById("firstNumber").value = Math.PI;
 	document.getElementById("secondNumber").value = Math.E;
